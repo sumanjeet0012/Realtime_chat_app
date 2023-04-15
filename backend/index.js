742c4d5374add7131348c8e4a165823a6b61585c
sumanjeet <sumanjeet0012@gmail.com>
2023-04-16 02:57:54 +0530

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -10,15 +11,14 @@ app.post("/authenticate", async (req, res) => {
 
   try {
 
-    const r = axios.PUT('https://api.chatengine.io/users/',
+    const r = await axios.put('https://api.chatengine.io/users/',
     {username: username , secret: username, first_name: username},
     {headers: {"private-key": "71830928-e5b9-46d7-a174-939fe452e490"}}
     )
     return res.status(r.status).json(r.data)
   } catch(e){
-    return res.status(e.responce.status).json(e.responce.data)
+    return res.status(e.response.status).json(e.response.data);
   }
-  return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
