fd5d4107778a09920459e8263f82afb72dae5d91
sumanjeet <sumanjeet0012@gmail.com>
2023-04-16 02:34:05 +0530

    create or update user code test - 1

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  try {
+
+    const r = axios.PUT('https://api.chatengine.io/users/',
+    {username: username , secret: username, first_name: username},
+    {headers: {"private-key": "71830928-e5b9-46d7-a174-939fe452e490"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch(e){
+    return res.status(e.responce.status).json(e.responce.data)
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
