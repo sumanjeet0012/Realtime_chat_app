9663f69dd72241b1024deb07db964428f220f9d0
sumanjeet <sumanjeet0012@gmail.com>
2023-04-16 14:13:40 +0530

    Project Completed

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '65185de6-ec1e-4cbf-b793-778c7bac22f1',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh'}}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        </div>
+    )
 }
 
 export default ChatsPage;
