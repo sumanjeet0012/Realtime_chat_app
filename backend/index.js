4f4f02a1c5c8973207c1e15c76051b11f7ae6728
sumanjeet <sumanjeet0012@gmail.com>
2023-04-22 11:18:15 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+require('dotenv').config()
+// console.log(process.env.API_KEY)
+const apikey=process.env.API_KEY
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
 
@@ -13,7 +16,8 @@ app.post("/authenticate", async (req, res) => {
 
     const r = await axios.put('https://api.chatengine.io/users/',
     {username: username , secret: username, first_name: username},
-    {headers: {"private-key": "71830928-e5b9-46d7-a174-939fe452e490"}}
+    {headers: {"private-key":process.env.API_KEY}}
+    // {headers: {"private-key": "71830928-e5b9-46d7-a174-939fe452e490"}}
     )
     return res.status(r.status).json(r.data)
   } catch(e){
